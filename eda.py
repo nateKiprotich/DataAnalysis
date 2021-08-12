7502a787628dcde0b91c37798e185a5ad0aa1dc9
Nate <nate.kiprotich@gmail.com>
2021-08-12 11:47:59 +0300

    Minor changes on EDA

--- eda.py
+++ eda.py
@@ -1,4 +1,5 @@
 import read_data as rd
+from scipy import stats
 
 
 df = rd.read_csv()
@@ -16,3 +17,24 @@ drive_wheels_counts.index.name="drive-wheels"
 print(100*"*")
 
 print(drive_wheels_counts)
+
+
+
+# Group By
+df["price"].replace("?", 0, inplace=True)
+df["price"] = df["price"].astype(float)
+
+df_test = df[["drive-wheels","body-style", "price"]]
+df_grp = df_test.groupby(["drive-wheels", "body-style"], as_index=False).mean()
+print(df_grp)
+
+print(df["price"].dtype)
+
+
+
+# Analysis of Variance (ANOVA)
+df_annova = df[["make", "price"]]
+grouped_annova =df_annova.groupby(["make"])
+annova_results_1 = stats.f_oneway(grouped_annova.get_group("honda")["price"], grouped_annova.get_group("subaru")["price"])
+
+print(annova_results_1)
